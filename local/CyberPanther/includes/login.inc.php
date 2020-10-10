2ce221449b94a27cad81a3b6700291328d9e52b2
Holleyj0831 <66688351+Holleyj0831@users.noreply.github.com>
2020-10-10 08:33:29 -0400

    Add files via upload
    local host

---
+++ local/CyberPanther/includes/login.inc.php
@@ -0,0 +1,56 @@
+<?php
+
+if (isset($_POST['login-submit'])) {
+
+    require 'dbh.inc.php';                                                          //uses file
+
+    //variables
+    $mailuid = $_POST['mailuid'];                                                   //id
+    $password = $_POST['pwd'];                                                      //password
+
+    if (empty($mailuid) || empty($password)) {                  
+        header("Location: ../login.html?error=emptyfields");                         //error checking for no input
+        exit();
+    }
+    else {
+        $sql = "SELECT * FROM users WHERE uidUsers=? OR emailUsers=?;";             //uses user id or email to identify      
+        $stmt = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($stmt, $sql)) {                                    //if the connection is succes 
+            header("Location: ../login.html?error=sqlerror");                        //calls error from                            
+            exit();                                                                 //exit code
+        }
+        else {
+
+            mysqli_stmt_bind_param($stmt, "ss", $mailuid, $mailuid);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+            if ($row = mysqli_fetch_assoc($result)) {                              //if result is found in row
+                $pwdCheck = password_verify($password, $row['pwdUsers']);          //check password to authenticate user
+                if ($pwdCheck == false) {                                          //if not found in table 
+                    header("Location: ../login.html?error=wrongpassword");          //calls error from file 
+                    exit();                                                        //exit code
+
+                }
+                else if ($pwdCheck == true) {                                      //if password is succesful found  
+                    session_start();                                               //start the session 
+                    $_SESSION['userID'] = $row['idUsers'];                         //display user 
+                    $_SESSION['userUid'] = $row['uidUsers'];                       
+
+                    header("Location: ../bag.html?login=success");                //call success from index file 
+                }
+                else {
+                    header("Location: ../login.html?error=wrongpwd");                //calls wrong password error 
+                    exit();                                                         //exit
+                }
+            }
+            else {
+                header("Location: ../login.html?error=nouser");                      //user doesnt exits error from index file 
+        exit();
+            }
+        }
+    }
+}
+else {
+    header("Location: ../index.html");
+        exit();
+}
---
+++ local/CyberPanther/includes/register.inc.php
@@ -0,0 +1,76 @@
+<?php
+if (isset($_POST['register-submit'])) {
+
+    require 'dbh.inc.php';
+
+    //variables
+    $username = $_POST['uid'];                //input username username = input
+    $email = $_POST['email'];                 //input
+    $studentID = $_POST['sid'];                 //input
+    $password = $_POST['pwd'];                  //input 
+    $passwordRepeat = $_POST['pwd-repeat'];     //input
+
+    if (empty($username) || empty($email) ||empty($studentID) ||empty($password) ||empty($passwordRepeat)) {        //if all fields are empty 
+        header("Location: ../login.html?error=emptyfields");                                                      //calls empty fields error from register file
+        exit();
+    }
+    else if (!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9]*$/", $username)) {           //if the email is not valid and doesnt match username     
+        header("Location: ../login.html?error=invalidemailuid");                                                  //call error from register file
+        exit();
+    }
+    else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {                          // if the email is not valid
+        header("Location: ../login.html?error=invalidemail");                     //call invalid email from register file 
+        exit();
+    }
+    else if (!preg_match("/^[a-zA-Z0-9]*$/", $username)) {                          //if the username is not valid 
+        header("Location: ../login.html?error=invaliduid");                       //call invalid username from register file 
+        exit();
+    }
+    else if ($password !== $passwordRepeat) {                                       //if the password is not equal to the the repeated password
+        header("Location: ../login.html?error=passwordcheck&uid=" .$username. "&mail=" .$email);                    //calls passwords are not the same error from file
+        exit();
+    }
+    else {
+
+        $sql = "SELECT uidUSERS FROM users WHERE uidUsers=? AND sidUsers=?";                        //$sql = "SELECT uidUSERS FROM users WHERE uidUsers=? AND sidUsers=?";  $sql = "SELECT * FROM users WHERE uidUsers=?;";
+        $stmt = mysqli_stmt_init($conn);                                            //initializes statement as an object for mysqli stmst prepare
+        if (!mysqli_stmt_prepare($stmt, $sql)) {
+            header("Location: ../login.html?error=sqlerror");                     
+            exit();
+        }
+        else {
+            mysqli_stmt_bind_param($stmt, "ss", $username, $studentID);             //connects variables to a parameter                             
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_store_result($stmt);                                        //store results for statement
+            $resultCheck = mysqli_stmt_num_rows($stmt);
+            if ($resultCheck > 0) {                                                     //if there is more than one occurance of the parameters
+                header("Location: ../register.index.html?error=uidtakensidtaken");             //call error username or id is taken 
+                exit();
+            }
+            else {
+
+                $sql = "INSERT INTO users (uidUsers, emailUsers, sidUsers, pwdUsers) VALUES (?, ?, ?, ?)";
+                if (!mysqli_stmt_prepare($stmt, $sql)) {                                                //if sql statement cannot prepare for execution     
+                    header("Location: ../login.html?error=sqlerror");                                 //call sql error from file 
+                    exit();
+                }
+                else {
+                    $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
+
+                    mysqli_stmt_bind_param($stmt, "ssss", $username, $email, $studentID, $hashedPwd);           //connects variables to a parameter
+                    mysqli_stmt_execute($stmt);                                                                 //
+                    header("Location: ../login.html");                                        //calls success message from file 
+                    exit();
+                }
+
+            }
+        }
+    }
+    mysqli_stmt_close($stmt);               //close variables
+    mysqli_close($conn);                    //close connection
+
+}
+else {
+    header("Location: ../login.html?");
+    exit();
+}
